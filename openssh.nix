4d139df894d8cdfd350339d4c916323d34b30ed4
Vanilla <osu_Vanilla@126.com>
2022-10-04 16:56:39 +0800

    chore[openssh.nix]: Put keyFiles to this file, add root for deploy-rs.

--- openssh.nix
+++ openssh.nix
@@ -1,5 +1,13 @@
-{ ... }:
-{
+{ lib, pkgs, ... }:
+rec {
   services.openssh.enable = true;
   services.openssh.permitRootLogin = "no";
+
+  users.users."vanilla".openssh.authorizedKeys.keyFiles = lib.singleton (pkgs.fetchurl {
+    url = "https://github.com/VergeDX.keys"; # Yubikeys 5 NFC
+    hash = "sha256-ASglsi7Ape5I9MabvIiWXPI7IeumBaoVTb+pX3Hm5EA=";
+  });
+
+  users.users."root".openssh.authorizedKeys.keyFiles =
+    users.users."vanilla".openssh.authorizedKeys.keyFiles;
 }
--- users.nix
+++ users.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   users.users."vanilla" = {
     isNormalUser = true;
@@ -9,11 +9,6 @@
     hashedPassword = "$6" + "$NixOS/Linode" +
       "$9wP2TPxYcCHdt5gQL5UgGeBUWkdLRndxCQqHEnVS8xC5CiJhmZY0UA"
       + ".7hsaKWtOmaHnM1llFqqGOJrtj0hOCA/";
-
-    openssh.authorizedKeys.keyFiles = lib.singleton (pkgs.fetchurl {
-      url = "https://github.com/VergeDX.keys"; # Yubikeys 5 NFC
-      hash = "sha256-ASglsi7Ape5I9MabvIiWXPI7IeumBaoVTb+pX3Hm5EA=";
-    });
   };
 
   users.mutableUsers = false;
