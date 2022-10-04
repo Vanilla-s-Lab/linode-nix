b31c5d78b69afed3a882c52a9a034bfed712cb62
Vanilla <osu_Vanilla@126.com>
2022-10-04 19:03:02 +0800

    chore[network.nix]: Adding options to avoiding logs.

--- network.nix
+++ network.nix
@@ -2,4 +2,7 @@
 {
   networking.hostName = "NixOS-Linode";
   networking.firewall.allowPing = false;
+
+  # TODO: Check the firewall of Linode, prefer.
+  networking.firewall.logRefusedConnections = false;
 }
