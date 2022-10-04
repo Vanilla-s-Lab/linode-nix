e60e7eb3445a935e2e3511a50b47a5ac13d15c8e
Vanilla <osu_Vanilla@126.com>
2022-10-04 18:28:08 +0800

    chore[images.tf]: Fix image name by using locals.

--- images.tf
+++ images.tf
@@ -1,9 +1,14 @@
+locals {
+  # TODO: Edit this line when changing; or use "local-exec".
+  img_name = "nixos-image-22.05.20221003.81a3237-x86_64-linux.img.gz"
+}
+
 // noinspection MissingProperty
 resource "linode_image" "nixos" {
   label = "nixos"
 
-  file_path = "result/nixos.img.gz"
-  file_hash = filemd5("result/nixos.img.gz")
+  file_path = "result/${local.img_name}"
+  file_hash = filemd5("result/${local.img_name}")
 
   // TODO: Use "ap-northeast" instead.
   region = "us-east"
