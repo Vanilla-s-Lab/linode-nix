9768e763b8fb1145d55e7517bbb54f9a075416b4
Vanilla <osu_Vanilla@126.com>
2022-10-04 20:29:56 +0800

    chore[network.nix]: Remove unused NAT, route only.

--- network.nix
+++ network.nix
@@ -6,10 +6,6 @@
   # TODO: Check the firewall of Linode, prefer.
   networking.firewall.logRefusedConnections = false;
 
-  networking.nat.enable = true;
-  networking.nat.externalInterface = "eth0";
-  networking.nat.internalInterfaces = [ "wg0" ];
-
   networking.firewall = {
     allowedUDPPorts = [ 51820 ];
   };
