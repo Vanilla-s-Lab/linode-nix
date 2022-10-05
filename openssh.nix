f73adc2c62a830c7a01ed27c534adae881e161c9
Vanilla <osu_Vanilla@126.com>
2022-10-05 13:24:34 +0800

    chore[users.nix]: Remove user vanilla, root with fish only.

--- openssh.nix
+++ openssh.nix
@@ -4,11 +4,8 @@ rec {
   # TODO: Disable all methods except pubkey.
   # services.openssh.permitRootLogin = "no";
 
-  users.users."vanilla".openssh.authorizedKeys.keyFiles = lib.singleton (pkgs.fetchurl {
+  users.users."root".openssh.authorizedKeys.keyFiles = lib.singleton (pkgs.fetchurl {
     url = "https://github.com/VergeDX.keys"; # Yubikeys 5 NFC
     hash = "sha256-ASglsi7Ape5I9MabvIiWXPI7IeumBaoVTb+pX3Hm5EA=";
   });
-
-  users.users."root".openssh.authorizedKeys.keyFiles =
-    users.users."vanilla".openssh.authorizedKeys.keyFiles;
 }
--- users.nix
+++ users.nix
@@ -1,14 +1,7 @@
 { pkgs, ... }:
 {
-  users.users."vanilla" = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-
+  users.users."root" = {
     shell = pkgs.fish;
-
-    hashedPassword = "$6" + "$NixOS/Linode" +
-      "$9wP2TPxYcCHdt5gQL5UgGeBUWkdLRndxCQqHEnVS8xC5CiJhmZY0UA"
-      + ".7hsaKWtOmaHnM1llFqqGOJrtj0hOCA/";
   };
 
   users.mutableUsers = false;
