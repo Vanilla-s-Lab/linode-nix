6da58a87c0748a1ea9e6690c6cf3ce5e2c81303a
Vanilla <osu_Vanilla@126.com>
2022-10-05 13:00:01 +0800

    feat[network.nix]: Settings DNS, Google + CF.

--- network.nix
+++ network.nix
@@ -14,4 +14,12 @@
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
   };
+
+  # https://developers.google.com/speed/public-dns/docs/using
+  # https://www.cloudflare.com/learning/dns/what-is-1.1.1.1/
+  networking.nameservers = [
+    "8.8.8.8"
+    "8.8.4.4"
+    "1.1.1.1"
+  ];
 }
