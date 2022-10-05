0aa6dabf8a55b7c560f6e2ba6e861ec7c603695c
Vanilla <osu_Vanilla@126.com>
2022-10-05 10:15:56 +0800

    fix[firewall.tf]: Allow 67-68 UDP for DHCP!

--- firewall.tf
+++ firewall.tf
@@ -2,6 +2,16 @@ resource "linode_firewall" "nixos" {
   label   = "nixos"
   linodes = [linode_instance.nixos.id]
 
+  // noinspection MissingProperty
+  inbound {
+    label    = "allow-dhcp"
+    protocol = "UDP"
+    ports    = "67-68"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+    action   = "ACCEPT"
+  }
+
   inbound_policy  = "DROP"
   outbound_policy = "ACCEPT"
 
