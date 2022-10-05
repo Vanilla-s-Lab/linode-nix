b09091f7bb127c8fdbbdd3bd30cf82f2b2458183
Vanilla <osu_Vanilla@126.com>
2022-10-05 11:13:58 +0800

    feat[shadowsocks.yaml]: Settings up secrets of password.

--- shadowsocks.nix
+++ shadowsocks.nix
@@ -1,7 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   environment.systemPackages = [
     # https://github.com/Mic92/sops-nix
     pkgs.ssh-to-pgp
   ];
+
+  # https://my.norton.com/extspa/passwordmanager?path=pwd-gen
+  sops.secrets."shadowsocks/password".sopsFile = ./shadowsocks.yaml;
+  sops.templates."shadowsocks".content = builtins.toJSON {
+    password = config.sops.placeholder."shadowsocks/password";
+  };
 }
