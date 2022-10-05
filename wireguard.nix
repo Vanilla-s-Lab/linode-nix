944294a6521de6cd2d5ddc918376b0abef8e1061
Vanilla <osu_Vanilla@126.com>
2022-10-05 08:46:33 +0800

    feat[wireguard.nix]: Self func mkPeers.

--- wireguard.nix
+++ wireguard.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }:
+let mkPeers = name: pk: ips: { publicKey = pk; allowedIPs = ips; }; in
 {
   environment.systemPackages = [
     pkgs.wireguard-tools
@@ -14,10 +15,13 @@
       privateKeyFile = "/root/wireguard-keys/private";
 
       peers = [
-        {
-          publicKey = "A5Acrw7cwNBcw1h5/L/qKmaSPknGqwFMZAMyjcp4TyA=";
-          allowedIPs = [ "10.100.0.2/32" ];
-        }
+        (mkPeers "NixOS-RoT" "A5Acrw7cwNBcw1h5/L/qKmaSPknGqwFMZAMyjcp4TyA=" [ "10.100.0.2/32" ])
+
+        # (mkPeers "Redmi-K30" "PSZJtkR1gUBUIw/SNRSAz3K/4BZCnoxJ8U2A757sAj4=" [ "10.100.0.3/32" ])
+        # (mkPeers "Xiaomi-Pad-5" "U22iFFBctYIEzhAawMTCvWztqPI3gkYJdpkWni/nOQ0=" [ "10.100.0.4/32" ])
+
+        # (mkPeers "Caetrix" "JDr3xRNUeBOtBqvdkluQtF8i1laHnLgrc5Afto1uIhs=" [ "10.100.0.5/32" ])
+        # (mkPeers "Reliena" "HYoNq/B7/uC/d7yCD0N8o1IIZOOKLceHxTd61m3ksQQ=" [ "10.100.0.6/32" ])
       ];
     };
   };
