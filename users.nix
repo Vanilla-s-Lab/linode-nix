329415d048f575fa3253295c8f5abefb5e9e01bf
Vanilla <osu_Vanilla@126.com>
2022-10-05 14:11:09 +0800

    fix[users.nix]: Add password for root user, for LISH.

--- users.nix
+++ users.nix
@@ -2,6 +2,10 @@
 {
   users.users."root" = {
     shell = pkgs.fish;
+
+    hashedPassword = "$6" + "$NixOS/Linode" +
+      "$9wP2TPxYcCHdt5gQL5UgGeBUWkdLRndxCQqHEnVS8xC5CiJhmZY0UA"
+      + ".7hsaKWtOmaHnM1llFqqGOJrtj0hOCA/";
   };
 
   users.mutableUsers = false;
