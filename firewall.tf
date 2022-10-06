b4d6c8bf2d56fb30587a7c8ec6019e064d0ae314
Vanilla <osu_Vanilla@126.com>
2022-10-06 10:26:05 +0800

    feat[nginx.nix]: Basic config for ACME certs.

--- firewall.tf
+++ firewall.tf
@@ -24,4 +24,24 @@ resource "linode_firewall" "nixos" {
     ipv6     = ["::/0"]
     action   = "ACCEPT"
   }
+
+  // noinspection MissingProperty
+  inbound {
+    label    = "accept-inbound-HTTP"
+    protocol = "TCP"
+    ports    = "80"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+    action   = "ACCEPT"
+  }
+
+  // noinspection MissingProperty
+  inbound {
+    label    = "accept-inbound-HTTPS"
+    protocol = "TCP"
+    ports    = "443"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+    action   = "ACCEPT"
+  }
 }
