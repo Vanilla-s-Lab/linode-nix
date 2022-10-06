2fb5b6676ffdf4cab269f166f85da23671482f03
Vanilla <osu_Vanilla@126.com>
2022-10-06 10:32:48 +0800

    chore[network.nix]: Remove legancy wg config in this file.

--- network.nix
+++ network.nix
@@ -3,18 +3,6 @@
   networking.hostName = "NixOS-Linode";
   networking.firewall.allowPing = false;
 
-  networking.firewall = {
-    allowedUDPPorts = [ 51820 ];
-  };
-
-  networking.firewall.trustedInterfaces = [
-    "wg0"
-  ];
-
-  boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = 1;
-  };
-
   # https://developers.google.com/speed/public-dns/docs/using
   # https://www.cloudflare.com/learning/dns/what-is-1.1.1.1/
   # https://en.wikipedia.org/wiki/1.1.1.1
