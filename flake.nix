234f7ff371c6d316a029e9f53e9e8dd57ceb0caf
Vanilla <osu_Vanilla@126.com>
2022-12-31 17:58:00 +0800

    refactor[flake.nix]: Import flake-utils, redo drv.

--- flake.nix
+++ flake.nix
@@ -1,25 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, ... }:
-    let system = "x86_64-linux"; in
-    rec {
-      pkgs = import nixpkgs {
-        inherit system;
-      };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      rec {
+        linode = nixpkgs.lib.nixosSystem {
+          inherit system;
 
-      linode = nixpkgs.lib.nixosSystem {
-        inherit system;
+          modules = [{ system.stateVersion = "22.11"; }]
+            ++ [ ./system/users.nix ./system/network.nix ./services/openssh.nix ] ++ [
+            "${nixpkgs}/nixos/modules/virtualisation/linode-config.nix"
+            "${nixpkgs}/nixos/modules/virtualisation/linode-image.nix"
+          ] ++ [ ./services/fail2ban.nix ./services/nginx.nix ];
+        };
 
-        modules = [{ system.stateVersion = "22.11"; }]
-          ++ [ ./system/users.nix ./system/network.nix ./services/openssh.nix ] ++ [
-          "${nixpkgs}/nixos/modules/virtualisation/linode-config.nix"
-          "${nixpkgs}/nixos/modules/virtualisation/linode-image.nix"
-        ] ++ [ ./services/fail2ban.nix ./services/nginx.nix ];
-      };
-
-      linode-image = linode.config.system.build.linodeImage;
-    };
+        # nix build .#linode-image.x86_64-linux -v -L
+        linode-image = linode.config.system.build.linodeImage;
+      }
+    );
 }
