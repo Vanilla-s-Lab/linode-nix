3928f8dc2a98504db4112890b65a74d2a5c01c53
Vanilla <osu_Vanilla@126.com>
2023-01-03 16:22:31 +0800

    refactor[flake.nix]: Remove list ++, use one list.

--- flake.nix
+++ flake.nix
@@ -15,11 +15,18 @@
       linode = nixpkgs.lib.nixosSystem {
         inherit system;
 
-        modules = [{ system.stateVersion = "22.11"; }]
-          ++ [ ./system/users.nix ./system/network.nix ./services/openssh.nix ] ++ [
+        modules = [
+          { system.stateVersion = "22.11"; }
+
+          ./system/users.nix
+          ./system/network.nix
+
           "${nixpkgs}/nixos/modules/virtualisation/linode-config.nix"
           "${nixpkgs}/nixos/modules/virtualisation/linode-image.nix"
-        ] ++ [ ./services/fail2ban.nix ];
+
+          ./services/openssh.nix
+          ./services/fail2ban.nix
+        ];
       };
 
       # nix build .#linode-image.x86_64-linux -v -L
