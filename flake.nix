733d76b936a6c9d91e4ca4ad7a9fa2b8d6ea8cf7
Vanilla <osu_Vanilla@126.com>
2023-01-03 16:18:02 +0800

    chore[flake.nix]: Remove flake-utils, add deploy-rs.

--- flake.nix
+++ flake.nix
@@ -1,25 +1,37 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    flake-utils.url = "github:numtide/flake-utils";
+
+    deploy-rs.url = "github:serokell/deploy-rs";
+    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      rec {
-        linode = nixpkgs.lib.nixosSystem {
-          inherit system;
+  outputs = { nixpkgs, deploy-rs, ... }:
 
-          modules = [{ system.stateVersion = "22.11"; }]
-            ++ [ ./system/users.nix ./system/network.nix ./services/openssh.nix ] ++ [
-            "${nixpkgs}/nixos/modules/virtualisation/linode-config.nix"
-            "${nixpkgs}/nixos/modules/virtualisation/linode-image.nix"
-          ] ++ [ ./services/fail2ban.nix ];
-        };
+    let system = "x86_64-linux"; in
+    let pkgs = nixpkgs.legacyPackages.${system}; in
 
-        # nix build .#linode-image.x86_64-linux -v -L
-        linode-image = linode.config.system.build.linodeImage;
-      }
-    );
+    rec {
+      linode = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [{ system.stateVersion = "22.11"; }]
+          ++ [ ./system/users.nix ./system/network.nix ./services/openssh.nix ] ++ [
+          "${nixpkgs}/nixos/modules/virtualisation/linode-config.nix"
+          "${nixpkgs}/nixos/modules/virtualisation/linode-image.nix"
+        ] ++ [ ./services/fail2ban.nix ];
+      };
+
+      # nix build .#linode-image.x86_64-linux -v -L
+      linode-image = linode.config.system.build.linodeImage;
+
+      deploy.nodes.linode = {
+        sshUser = "root";
+        hostname = "172.105.209.227";
+
+        profiles.system.path =
+          deploy-rs.lib."${system}".activate.nixos
+            linode;
+      };
+    };
 }
