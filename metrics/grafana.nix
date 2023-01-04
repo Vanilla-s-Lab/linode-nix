1818e986f7d401413ea0f2e092126c22d2a04b8d
Vanilla <osu_Vanilla@126.com>
2023-01-04 17:06:57 +0800

    fix[metrics/grafana.nix]: Also proxy grafana api endpoint.

--- metrics/grafana.nix
+++ metrics/grafana.nix
@@ -7,18 +7,30 @@
     server.domain = "grafana.vergedx.me";
   };
 
+  services.nginx.upstreams."grafana".servers = {
+    "127.0.0.1:3000" = { };
+  };
+
   services.nginx.virtualHosts = {
     "grafana.vergedx.me" = {
       addSSL = true;
       enableACME = true;
 
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:3000";
+      # https://grafana.com/tutorials/run-grafana-behind-a-proxy/
+      # https://github.com/yandex/gixy/blob/master/docs/en/plugins/hostspoofing.md
 
-        # https://grafana.com/tutorials/run-grafana-behind-a-proxy/
-        # https://github.com/yandex/gixy/blob/master/docs/en/plugins/hostspoofing.md
-        extraConfig = "proxy_set_header Host" + " " + /* "$http_host" */ "$host" + ";";
-      };
+      locations."/".extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_pass http://grafana;
+      '';
+
+      locations."/api/live/".extraConfig = ''
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection $connection_upgrade;
+        proxy_set_header Host $host;
+        proxy_pass http://grafana;
+      '';
     };
   };
 }
