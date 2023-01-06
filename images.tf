86d01563cb2f8a7504eaa738b134da89ecb2c1f7
Vanilla <osu_Vanilla@126.com>
2023-01-06 17:36:14 +0800

    chore[images.tf]: Pin the file_path & file_hash, src is removed.

--- images.tf
+++ images.tf
@@ -1,14 +1,12 @@
-locals {
-  // https://www.terraform.io/language/functions/fileset
-  img_files = fileset(path.module, "result/*.img.gz")
-}
-
 resource "linode_image" "nixos" {
   label = "nixos"
 
   // https://stackoverflow.com/questions/66069677/get-an-element-from-a-list-terraform
-  file_path = tolist(local.img_files)[0]
-  file_hash = filemd5(tolist(local.img_files)[0])
+  file_path = "result/nixos-image-22.11.20221231.feda52b-x86_64-linux.img.gz"
+  file_hash = "a034ce957205d565539d6f904c5ef207"
 
   region = "us-east"
 }
+
+output "file_path" { value = linode_image.nixos.file_path }
+output "file_hash" { value = linode_image.nixos.file_hash }
