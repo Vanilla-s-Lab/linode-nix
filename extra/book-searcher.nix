665273d942ff335f0606499a3ae762f11855fcbc
Vanilla <osu_Vanilla@126.com>
2023-01-11 13:12:44 +0800

    feat[book-searcher.nix]: Use self built images with index.

--- extra/book-searcher.nix
+++ extra/book-searcher.nix
@@ -5,10 +5,9 @@
     pkgs.unzip
   ];
 
-  # https://github.com/zlib-searcher/zlib-searcher/blob/master/docker-compose.yml
+  # https://github.com/orgs/Vanilla-s-Lab/packages?repo_name=collections
   virtualisation.oci-containers.containers."book-searcher" = {
-    image = "ghcr.io/book-searcher-org/book-searcher:0.8.0";
+    image = "ghcr.io/vanilla-s-lab/book-searcher:0.8.0";
     ports = lib.singleton "127.0.0.1:7070:7070";
-    volumes = lib.singleton "/root/index:/index";
   };
 }
