df161c4535e8c5e32ec8a1fb6caa7c2723015c55
Vanilla <osu_Vanilla@126.com>
2023-01-12 16:19:36 +0800

    update[extra/book-searcher.nix]: Container 0.8.1 -> 0.8.2.

--- extra/book-searcher.nix
+++ extra/book-searcher.nix
@@ -7,7 +7,7 @@
 
   # https://github.com/orgs/Vanilla-s-Lab/packages?repo_name=collections
   virtualisation.oci-containers.containers."book-searcher" = {
-    image = "ghcr.io/vanilla-s-lab/book-searcher:0.8.1";
+    image = "ghcr.io/vanilla-s-lab/book-searcher:0.8.2";
     ports = lib.singleton "127.0.0.1:7070:7070";
   };
 }
