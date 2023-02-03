13b0314b5142594952e2a1a173d18c32d8f56f48
Vanilla <osu_Vanilla@126.com>
2023-02-03 12:48:31 +0800

    feat[network.nix]: Add dummy device of self dn42 ASN.

--- system/network.nix
+++ system/network.nix
@@ -2,4 +2,21 @@
 {
   networking.hostName = "NixOS-Linode";
   networking.firewall.allowPing = false;
+
+  # https://serverfault.com/questions/1029041/how-to-create-dummy-interface-in-etc-systemd-network
+
+  systemd.network.enable = true;
+
+  systemd.network.netdevs."dummy" = {
+    netdevConfig = { Name = "dummy"; Kind = "dummy"; };
+  };
+
+  systemd.network.networks."dummy" = {
+    matchConfig = { Name = "dummy"; };
+
+    address = [
+      "172.22.130.97"
+      "fd13:f622:f715::1"
+    ];
+  };
 }
