9d19a52b3689e0758eb564d0d5900ccea652bd0b
Vanilla <osu_Vanilla@126.com>
2023-02-06 09:29:08 +0800

    feat[services/pykms.nix]: Add subdomain for a default page.

--- services/pykms.nix
+++ services/pykms.nix
@@ -1,5 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.pykms.enable = true;
   services.pykms.openFirewallPort = true;
+
+  services.nginx.virtualHosts = {
+    "pykms.vergedx.me" = {
+      addSSL = true;
+      enableACME = true;
+
+      # https://nixos.wiki/wiki/Nginx
+      root = "${pkgs.nginx}/html";
+    };
+  };
 }
