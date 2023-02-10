1ac1e3f40d77625ca99c4784234f91ed78b43300
Vanilla <osu_Vanilla@126.com>
2023-02-10 13:16:35 +0800

    fix[metrics/csgo_exporter.nix]:  Double scrape interval again.

--- metrics/csgo_exporter.nix
+++ metrics/csgo_exporter.nix
@@ -18,7 +18,7 @@ in
       # https://serverfault.com/questions/867830/systemd-start-service-only-after-dns-is-available
       "ExecStartPre" = "${pkgs.bash}/bin/bash -c 'until ${pkgs.host}/bin/host api.steampowered.com; do sleep 1; done'";
 
-      "ExecStart" = "${pkgs_csgo_exporter}/bin/csgo_exporter -steam_id 76561198319100373 -scrape_interval 8m";
+      "ExecStart" = "${pkgs_csgo_exporter}/bin/csgo_exporter -steam_id 76561198319100373 -scrape_interval 16m";
       "Restart" = "always";
 
       # https://flatcar-linux.org/docs/latest/setup/systemd/environment-variables/
